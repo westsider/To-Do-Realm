ab972473e1b95c79e213f3caf6e83ea38d42af93
Warren Hansen <whansen1@mac.com>
2017-08-29 19:03:14 -0700

    task: added Realm

--- Realm TooDo/ViewController.swift
+++ Realm TooDo/ViewController.swift
@@ -7,17 +7,75 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     
     @IBOutlet weak var tableview: UITableView!
     
+    let realm = try! Realm()
+    
+    var todoList: Results<TodoItem> {
+        get {
+            return realm.objects(TodoItem.self)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let item = todoList[indexPath.row]
+        
+        cell.textLabel!.text = item.detail
+        cell.detailTextLabel!.text = "\(item.status)"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoList.count
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let item = todoList[indexPath.row]
+        try! self.realm.write({
+            if (item.status == 0){
+                item.status = 1
+            }else{
+                item.status = 0
+            }
+        })
+        tableView.reloadRows(at: [indexPath], with: .automatic)
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    // [2]
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if (editingStyle == .delete){
+            let item = todoList[indexPath.row]
+            try! self.realm.write({
+                self.realm.delete(item)
+            })
+            
+            tableView.deleteRows(at:[indexPath], with: .automatic)
+            
+        }
+        
+    }
+    
     @IBAction func addNew(_ sender: Any) {
         
         let alertController : UIAlertController = UIAlertController(title: "New Todo", message: "What do you plan to do?", preferredStyle: .alert)
@@ -32,29 +90,25 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         alertController.addAction(action_cancel)
         
         let action_add = UIAlertAction.init(title: "Add", style: .default) { (UIAlertAction) -> Void in
+            let textField_todo = (alertController.textFields?.first)! as UITextField
+            print("You Entered \(textField_todo.text ?? "No Value")")
             
+            let todoItem = TodoItem() // [1]
+            todoItem.detail = textField_todo.text!
+            todoItem.status = 0
+            
+            try! self.realm.write({ // [2]
+                self.realm.add(todoItem)
+                self.tableview.insertRows(at: [IndexPath.init(row: self.todoList.count-1, section: 0)], with: .automatic)
+            })
         }
+        
         alertController.addAction(action_add)
         
         present(alertController, animated: true, completion: nil)
     }
 
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel!.text = "it works"
-        cell.detailTextLabel!.text = "on going"
-        return cell
-    }
-    
-    // [3]
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    // [4]
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
+
     
 }
 
